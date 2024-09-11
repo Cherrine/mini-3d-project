2cbc2192239628a2c9002b695f03e940e970156b
Cherrine <66070031@kmitl.ac.th>
2024-09-11 12:30:21 +0700

    ?

--- lab2/main.js
+++ lab2/main.js
@@ -7,26 +7,28 @@ function init() {
         scene.fog = new THREE.FogExp2(0xffffff, 0.2);
     }
 
-    var box = getBox(1, 1, 1);
     var plane = getPlane(20);
-    var pointLight = getPointLight(1);
+    var spotLight = getSpotLight(1); // Using SpotLight
     var sphere = getSphere(0.05);
     var boxGrid = getBoxGrid(10, 1.5);
 
     plane.name = 'plane-1';
 
-    box.position.y = box.geometry.parameters.height / 2;
+    
     plane.rotation.x = Math.PI / 2;
-    pointLight.position.y = 2;
+    spotLight.position.set(0, 4, 0); // Set spotLight position
+    spotLight.intensity = 2;
 
- 
     scene.add(plane);
-    pointLight.add(sphere);
-    scene.add(pointLight);
+    spotLight.add(sphere);
+    scene.add(spotLight);
     scene.add(boxGrid);
 
-    gui.add(pointLight, 'intensity', 0, 10);
-    gui.add(pointLight.position, 'y', 0, 5);
+    gui.add(spotLight, 'intensity', 0, 10);
+    gui.add(spotLight.position, 'x', 0, 20);
+    gui.add(spotLight.position, 'y', 0, 20);
+    gui.add(spotLight.position, 'z', 0, 20);
+    gui.add(spotLight, 'penumbra', 0, 1);
 
     var camera = new THREE.PerspectiveCamera(
         50,
@@ -36,7 +38,6 @@ function init() {
     );
     camera.position.set(1, 2, 5);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -49,82 +50,67 @@ function init() {
 }
 
 function getBox(width, height, depth) {
-
     var geometry = new THREE.BoxGeometry(width, height, depth);
     var material = new THREE.MeshPhongMaterial({
         color: 'rgb(120, 120, 120)'
     });
     var mesh = new THREE.Mesh(geometry, material);
-
     mesh.castShadow = true;
-
     return mesh;
 }
 
 function getPlane(size) {
-
     var geometry = new THREE.PlaneGeometry(size, size);
     var material = new THREE.MeshPhongMaterial({
         color: 'rgb(120, 120, 120)',
         side: THREE.DoubleSide
     });
     var mesh = new THREE.Mesh(geometry, material);
-
     mesh.receiveShadow = true;
-
     return mesh;
 }
 
-function getPointLight(intensity) {
-    var light = new THREE.PointLight(0xffffff, intensity);
-    
+function getSpotLight(intensity) { // Adjusted to only use SpotLight
+    var light = new THREE.SpotLight(0xffffff, intensity);
     light.castShadow = true;
-
+    light.angle = Math.PI / 6;
+    light.penumbra = 0.5;
+    light.decay = 2;
+    light.distance = 200;
     return light;
 }
 
 function update(renderer, scene, camera, controls) {
     renderer.render(scene, camera);
-
     controls.update();
-
     requestAnimationFrame(function () {
         update(renderer, scene, camera, controls);
     });
 }
 
-function getSphere(size){
+function getSphere(size) {
     var geometry = new THREE.SphereGeometry(size, 24, 24);
     var material = new THREE.MeshBasicMaterial({
         color: 'rgb(255, 255, 255)'
     });
-    var mesh = new THREE.Mesh(
-        geometry, material);
+    var mesh = new THREE.Mesh(geometry, material);
     return mesh;
 }
 
-function getBoxGrid(amount, seperationMultiplier){
+function getBoxGrid(amount, separationMultiplier) {
     var group = new THREE.Group();
-
-    for (var i = 0; i < amount; i++){
-        var obj = getBox(1, 1, 1);
-        obj.position.x = i * seperationMultiplier;
-        obj.position.y = obj.geometry.parameters.height / 2;
-        group.add(obj);
-        for (var j = 0; j < amount; j++){
+    for (var i = 0; i < amount; i++) {
+        for (var j = 0; j < amount; j++) {
             var obj = getBox(1, 1, 1);
-            obj.position.x = i * seperationMultiplier;
+            obj.position.x = i * separationMultiplier;
             obj.position.y = obj.geometry.parameters.height / 2;
-            obj.position.z = j * seperationMultiplier;
+            obj.position.z = j * separationMultiplier;
             group.add(obj);
         }
     }
-
-    group.position.x = -(seperationMultiplier * (amount - 1)) / 2;
-    group.position.z = -(seperationMultiplier * (amount - 1)) / 2;
-
+    group.position.x = -(separationMultiplier * (amount - 1)) / 2;
+    group.position.z = -(separationMultiplier * (amount - 1)) / 2;
     return group;
-
 }
 
 init();
